370dc51b3dd494d4770ae2efc7ae9d82ee17794c
Jerome Boyer <boyerje@us.ibm.com>
2019-02-25 20:39:59 -0800

    Separate build using nodetools image

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 export LDFLAGS=-L/usr/local/opt/openssl/lib
 export CPPFLAGS=-I/usr/local/opt/openssl/include
-set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
 if [[ $PWD = */scripts ]]; then
  cd ..
 fi
 
+tools=$(docker images | grep nodetools)
+if [[ -z "$tools" ]]
+then
+   cd ui && npm install && ng build && cd ../server && npm install && npm run build
+else
+   docker run -v $(pwd):/home -ti ibmcase/nodetools bash -c "cd /home/ui  && npm install && ng build && cd ../server && npm install && npm run build"
+fi
+# TODO add CA certificate for icp or iks deployment
+
 docker build -t ibmcase/kc-ui .
-# image for private registry in IBM Cloud
-docker tag ibmcase/kc-ui registry.ng.bluemix.net/ibmcaseeda/kc-ui
+if [[ $kcenv -ne "local"]]; then
+    # image for private registry in IBM Cloud
+    docker tag ibmcase/kc-ui registry.ng.bluemix.net/ibmcaseeda/kc-ui
+fi
