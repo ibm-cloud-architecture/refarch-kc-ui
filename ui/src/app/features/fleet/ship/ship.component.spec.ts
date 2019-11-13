7fb5dab0ed7aea53c778da24196214ed41c22a58
Jerome Boyer <boyerje@us.ibm.com>
2019-11-13 15:19:20 -0800

    fix issue on ship component basic tests

--- ui/src/app/features/fleet/ship/ship.component.spec.ts
+++ ui/src/app/features/fleet/ship/ship.component.spec.ts
@@ -2,20 +2,43 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShipComponent } from './ship.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Routes } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { SimulcontrolComponent } from '../../simulcontrol/simulcontrol.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FleetService } from '../fleet.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ShipComponent', () => {
   let component: ShipComponent;
   let fixture: ComponentFixture<ShipComponent>;
   let template: HTMLElement;
+  let fleetStub;
+
   const routes: Routes = []; 
 
   beforeEach(async(() => {
+    let ship = { 'name': 'shiptest','maxRow':4, 'maxColum':6, 'containers':[],'longitude': '37.8226902168957',
+    'latitude':'-122.3248956640928'};
+    fleetStub = jasmine.createSpyObj("fleetStub", ['getSelectedShip']);
+    fleetStub.getSelectedShip.and.returnValue(ship)
     TestBed.configureTestingModule({
-      declarations: [ ShipComponent ],
+      declarations: [ ShipComponent,SimulcontrolComponent ],
       imports: [
-        RouterTestingModule.withRoutes(routes)
+        RouterTestingModule.withRoutes(routes),
+        MatToolbarModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatIconModule,
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
       ],
+      providers: [  { provide: FleetService, useValue: fleetStub } ]
     })
     .compileComponents();
   }));
@@ -24,16 +47,14 @@ describe('ShipComponent', () => {
     fixture = TestBed.createComponent(ShipComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    template = fixture.nativeElement.querySelector('./ship.component.html');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should  create a ship', () => {
-    fixture.detectChanges();
-    expect(template).toBeDefined();
+  it('should  create a ship', () => {
+    expect(component.ship).toBeDefined();
   });
   
 });
