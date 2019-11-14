dc1f52bf2f4fd49b2229614be79d4bcdddf4d4dc
Darnele Adhemar <darnelepierrelouis@gmail.com>
2019-11-14 12:53:20 -0500

    fixed unit test

--- ui/src/app/features/fleet/ship/ship.component.spec.ts
+++ ui/src/app/features/fleet/ship/ship.component.spec.ts
@@ -5,15 +5,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Routes } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
-<<<<<<< HEAD
-import { MatSliderModule } from '@angular/material/slider';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FleetService } from '../fleet.service';
-
-fdescribe('ShipComponent', () => {
-=======
-import { SimulcontrolComponent } from '../../simulcontrol/simulcontrol.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,10 +13,8 @@ import { FleetService } from '../fleet.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ShipComponent', () => {
->>>>>>> 7fb5dab0ed7aea53c778da24196214ed41c22a58
   let component: ShipComponent;
   let fixture: ComponentFixture<ShipComponent>;
-  let template: HTMLElement;
   let fleetStub;
 
   const routes: Routes = []; 
@@ -36,30 +25,17 @@ describe('ShipComponent', () => {
     fleetStub = jasmine.createSpyObj("fleetStub", ['getSelectedShip']);
     fleetStub.getSelectedShip.and.returnValue(ship)
     TestBed.configureTestingModule({
-<<<<<<< HEAD
-      declarations: [ ShipComponent, SimulcontrolComponent ],
-=======
       declarations: [ ShipComponent,SimulcontrolComponent ],
->>>>>>> 7fb5dab0ed7aea53c778da24196214ed41c22a58
       imports: [
         RouterTestingModule.withRoutes(routes),
         MatToolbarModule,
         MatSelectModule,
         MatSliderModule,
         MatIconModule,
-<<<<<<< HEAD
-        FormsModule,
-        ReactiveFormsModule
-      ],
-      providers: [
-        FleetService,
-      { provide: ,  useValue: }
-=======
         HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
         BrowserAnimationsModule
->>>>>>> 7fb5dab0ed7aea53c778da24196214ed41c22a58
       ],
       providers: [  { provide: FleetService, useValue: fleetStub } ]
     })
@@ -77,12 +53,7 @@ describe('ShipComponent', () => {
   });
 
   it('should  create a ship', () => {
-<<<<<<< HEAD
-    fixture.detectChanges();
-    expect(template).toBeDefined();
-=======
     expect(component.ship).toBeDefined();
->>>>>>> 7fb5dab0ed7aea53c778da24196214ed41c22a58
   });
   
 });
--- ui/src/app/features/orders/order/order.component.spec.ts
+++ ui/src/app/features/orders/order/order.component.spec.ts
@@ -1,16 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OrderComponent } from './order.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+//import { Order } from '../Order';
 
-describe('OrderComponent', () => {
+
+fdescribe('OrderComponent', () => {
   let component: OrderComponent;
   let fixture: ComponentFixture<OrderComponent>;
+  let httpTestingController : HttpTestingController;
+  let httpClient : HttpClient;
 
   beforeEach(async(() => {
+    let order = { 'productId': '12ddd4', 'quantity': ' ', 'pickUpDate': ' ', 'expectedDeliveryDate': ' ',
+    'pickUpAddress': '', 'destinationAddress': ' '}
+
     TestBed.configureTestingModule({
-      declarations: [ OrderComponent ]
+      declarations: [ OrderComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        HttpClientTestingModule,
+        HttpTestingController,
+      ],
+      providers: 
     })
     .compileComponents();
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+
   }));
 
   beforeEach(() => {
@@ -22,4 +47,8 @@ describe('OrderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create an order', () => {
+    expect(component.order).toBeDefined();
+  })
 });
