349b82118e535f87dff3c1ad0e49c618ae41479d
Darnele Adhemar <darnelepierrelouis@gmail.com>
2019-12-19 20:24:07 -0500

    added latitude and longitude

--- ui/src/app/features/containers/mock-container.ts
+++ ui/src/app/features/containers/mock-container.ts
@@ -1,11 +1,11 @@
 import { Container } from './Container';
 export const CONTAINERS: Container[] = [
-    {id: '23pg4', type: 'reefer', temperature: 34, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 1, column: 1, shipId: '12kj09'},
-    {id: '23pg5', type: 'reefer', temperature: 38, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 2, column: 1, shipId: '12kj09'},
-    {id: '23pg6', type: 'reefer', temperature: 24, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 3, column: 1, shipId: '12kj09'},
-    {id: '23pg7', type: 'reefer', temperature: 29, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 4, column: 1, shipId: '12kj09'},
-    {id: '23pg8', type: 'reefer', temperature: 50, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 5, column: 1, shipId: '12kj09'},
-    {id: '23pg9', type: 'reefer', temperature: 12, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 6, column: 1, shipId: '12kj09'},
-    {id: '23pg10', type: 'reefer', temperature: 20, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 7, column: 1, shipId: '12kj09'},
-    {id: '23pg11', type: 'reefer', temperature: 28, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 8, column: 1, shipId: '12kj09'},
+    {id: '23pg4', type: 'reefer', temperature: 34, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 1, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg5', type: 'reefer', temperature: 38, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 2, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg6', type: 'reefer', temperature: 24, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 3, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg7', type: 'reefer', temperature: 29, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 4, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg8', type: 'reefer', temperature: 50, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 5, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg9', type: 'reefer', temperature: 12, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 6, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg10', type: 'reefer', temperature: 20, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 7, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
+    {id: '23pg11', type: 'reefer', temperature: 28, humidity: 24, co2: 5, amp: 27, status: 'Active', row: 8, column: 1, shipId: '12kj09', latitude: 456.8, longitude: 432.9},
   ];
