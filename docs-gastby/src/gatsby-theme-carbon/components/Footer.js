fab2618c779651b8b5e65b77ddb4c55611bdf6f5
Darnele Adhemar <darnelepierrelouis@gmail.com>
2020-03-19 11:18:37 -0400

    deleted unused files, minor modifications

--- docs-gastby/src/gatsby-theme-carbon/components/Footer.js
+++ docs-gastby/src/gatsby-theme-carbon/components/Footer.js
@@ -2,29 +2,10 @@ import React from 'react';
 import Footer from 'gatsby-theme-carbon/src/components/Footer';
 
 const Content = ({ buildTime }) => (
-  <>
-    {/* <p>
-      The <code>Content</code> component receives a <code>buildTime</code> prop
-      that to display your site's build time: {buildTime}
-    </p> */}
     <p>
       <h4>Contribute:</h4>
     As this implementation solution is part of the Event Driven architeture reference architecture, the contribution policies apply the same way <a href="https://github.com/ibm-cloud-architecture/refarch-kc-ui/blob/master/CONTRIBUTING.md">here</a>.
     </p>
-    {/* <p>
-      By importing the <strong>Footer</strong> component from
-      gatsby-theme-carbon, we can supply our own props.
-    </p>
-    <p>
-      The default export from a shadowed component will replace that component
-      in the theme.
-    </p> */}
-    {/* <p>
-      <a href="https://www.gatsbyjs.org/docs/themes/api-reference/#component-shadowing">
-        More about component shadowing
-      </a>
-    </p> */}
-  </>
 );
 
 const links = {
@@ -32,13 +13,8 @@ const links = {
     { linkText: 'Contributors:' },
     { href: 'https://www.linkedin.com/in/jeromeboyer/', linkText: 'Jerome Boyer' },
     { href: 'https://www.linkedin.com/in/hemankita-perabathini/', linkText: 'Hemankita Perabathini' },
+    { href: 'https://www.linkedin.com/in/darneleadhemar/', linkText: 'Darnèle Adhemar' },
   ],
-  // secondCol: [
-  //   { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-  //   { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-  //   { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-  //   { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-  // ],
 };
 
 const CustomFooter = () => <Footer  Content={Content} links={links} />;
--- docs-gastby/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ docs-gastby/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -6,14 +6,6 @@ const links = [
     title: 'Github',
     href: 'https://ibm-cloud-architecture.github.io/refarch-kc',
   },
-  // {
-  //   title: 'Carbon',
-  //   href: 'https://www.carbondesignsystem.com',
-  // },
-  // {
-  //   title: 'Configuration guide',
-  //   href: '/guides/configuration',
-  // },
 ];
 
 // shouldOpenNewTabs: true if outbound links should open in a new tab
